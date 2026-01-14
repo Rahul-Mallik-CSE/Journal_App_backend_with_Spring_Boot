bd115481fa04bc8ea8dc7283f3edfc6ff4665684
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-14 09:43:54 +0600

    first commit

---
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplications.java
@@ -0,0 +1,12 @@
+package net.engineeringdigest.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplications {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplications.class, args);
+    }
+}
