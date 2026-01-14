1dceac2fc47e696891fc43e9750af1faaafde4ba
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-14 10:33:40 +0600

    updated

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,27 @@
 package net.engineeringdigest.journalApp.controller;
 
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+
+    private Map<Long, JournalEntry> journalEntries =new HashMap();
+
+    @GetMapping("/abc")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+
 
 }
